00d50b329496fa2116ca4a561a98b90a0c881d07
JosuePercy <josuehuallullo12@gmail.com>
2023-06-06 11:02:10 -0500

    inyectar dependencies

--- src/bases/03-classes.ts
+++ src/bases/03-classes.ts
@@ -1,3 +1,6 @@
+import axios from "axios";
+import { Move, PokeapiResponse } from "../interface/pokeapi-response.interface";
+
 export class Pokemon {
   get imageURL(): string {
     return `http://pokemon.com/${this.id}.jpg`;
@@ -6,7 +9,8 @@ export class Pokemon {
   constructor(
     public id: number,
     public name: string // public imageURL: string
-  ) {}
+  ) // TODO: inyectar dependencies
+  {}
 
   scream() {
     console.log(`${this.name.toUpperCase()}!!!`);
@@ -16,16 +20,21 @@ export class Pokemon {
     console.log(`${this.name}, ${this.name}`);
   }
 
-  getMoves() {
-    return 10;
+  async getMoves(): Promise<Move[]> {
+    const { data } = await axios.get<PokeapiResponse>(
+      "https://pokeapi.co/api/v2/pokemon/4"
+    );
+    console.log(data.moves);
+    return data.moves;
   }
 }
 
 export const charmander = new Pokemon(4, "charmander");
 
-console.log(charmander);
+// console.log(charmander);
 
 // charmander.scream();
 // charmander.speak();
 
-console.log(charmander.getMoves());
+// console.log(charmander.getMoves());
+charmander.getMoves();
--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,6 @@ import "./style.css";
 import { charmander } from "./bases/03-classes";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-    <h1>Hello Vite! ${charmander.name}</h1>
+    <h1>Hello Vite! ${charmander.name} ${charmander.id}</h1
+    <p>linl${charmander.getMoves}</p>
 `;
